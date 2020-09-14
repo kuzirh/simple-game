479a3f573ecc836db3610777907b473a2f8fc835
kuzirh <hrizukmichael@gmail.com>
2020-09-14 18:05:28 -0400

    create start game screen and added basic styling

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './src/components/Header';
+import StartGameScreen from './src/screens/StartGameScreen';
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title='Guess a Number' />
+      <StartGameScreen />
     </View>
   );
 }
