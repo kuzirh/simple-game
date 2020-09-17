6dba92bd0c08eb174cc7f6a15809c1478138e59f
kuzirh <hrizukmichael@gmail.com>
2020-09-16 20:48:40 -0400

    list of past guess being shown on game screen now

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,8 +1,17 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import MainButton from '../components/MainButton';
+import { Ionicons } from '@expo/vector-icons';
+import BodyText from '../components/BodyText';
 const generateRandomBetween = (
   min: number,
   max: number,
@@ -18,17 +27,28 @@ const generateRandomBetween = (
   }
 };
 
+const renderListItem = (
+  listLength: number,
+  itemData: { index: number; item: string }
+) => (
+  <View style={styles.listItem}>
+    <BodyText style={{}}>#{listLength - itemData.index}</BodyText>
+    <BodyText style={{}}>{itemData.item}</BodyText>
+  </View>
+);
+
 const GameScreen = (props: { userChoice: number; onGameOver: any }) => {
-  const [currentGuess, setCurrentGuess] = useState<number>(
-    generateRandomBetween(1, 100, props.userChoice)
-  );
-  const [rounds, setRounds] = useState<number>(0);
+  const initialGuess = generateRandomBetween(1, 100, props.userChoice);
+  const [currentGuess, setCurrentGuess] = useState<number>(initialGuess);
+  const [pastGuesses, setSetPastGuesses] = useState<Array<string>>([
+    initialGuess.toString(),
+  ]);
   const currentLow = useRef<number>(1);
   const currentHigh = useRef<number>(100);
   const { userChoice, onGameOver } = props;
   useEffect(() => {
     if (currentGuess === userChoice) {
-      onGameOver(rounds);
+      onGameOver(pastGuesses.length);
     }
   }, [currentGuess, userChoice, onGameOver]);
   const nextGuessHandler = (direction: string) => {
@@ -47,7 +67,7 @@ const GameScreen = (props: { userChoice: number; onGameOver: any }) => {
     if (direction === 'lower') {
       currentHigh.current = currentGuess;
     } else {
-      currentLow.current = currentGuess;
+      currentLow.current = currentGuess + 1;
     }
     const nextNumber = generateRandomBetween(
       currentLow.current,
@@ -55,7 +75,10 @@ const GameScreen = (props: { userChoice: number; onGameOver: any }) => {
       currentGuess
     );
     setCurrentGuess(nextNumber);
-    setRounds((currentRounds) => currentRounds + 1);
+    setSetPastGuesses((currentPastGuesses) => [
+      nextNumber.toString(),
+      ...currentPastGuesses,
+    ]);
   };
   return (
     <View style={styles.screen}>
@@ -63,12 +86,20 @@ const GameScreen = (props: { userChoice: number; onGameOver: any }) => {
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
         <MainButton onHandleClick={() => nextGuessHandler('lower')}>
-          LOWER
+          <Ionicons name='md-remove' size={24} color='white' />
         </MainButton>
         <MainButton onHandleClick={() => nextGuessHandler('greater')}>
-          GREATER
+          <Ionicons name='md-add' size={24} color='white' />
         </MainButton>
       </Card>
+      <View style={styles.listContainer}>
+        <FlatList
+          keyExtractor={(item) => item}
+          data={pastGuesses}
+          renderItem={(item) => renderListItem(pastGuesses.length, item)}
+          contentContainerStyle={styles.list}
+        />
+      </View>
     </View>
   );
 };
@@ -84,6 +115,25 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 400,
     maxWidth: '90%',
+  },
+  listItem: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '60%',
+  },
+  listContainer: {
+    width: '80%',
+    flex: 1,
+  },
+  list: {
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flexGrow: 1,
   },
 });
 export default GameScreen;
